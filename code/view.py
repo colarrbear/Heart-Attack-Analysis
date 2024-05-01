84043bfbdd61089b7e8430b67d9e3f3b884c961c
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-05-02 04:52:57 +0700

    simply page

--- code/view.py
+++ code/view.py
@@ -1,214 +1,4 @@
-# """handles the GUI"""
-# # from tkinter import ttk  # Normal Tkinter.* widgets are not themed!
-# # from ttkthemes import ThemedTk
-#
-# # window = ThemedTk(theme="blue")
-# # ttk.Button(window, text="Quit", command=window.destroy).pack()
-# # window.mainloop()
-# import tkinter as tk
-# from tkinter import ttk
-# from ttkthemes import ThemedTk
-# from model import *
-#
-#
-# class HeartDiseaseView(tk.Tk):
-#     def __init__(self, controller):
-#         super().__init__()
-#         self.controller = controller
-#         self.title("Heart Disease Explorer")
-#         self.init_components()
-#         self.init_home_page()
-#
-#     def init_components(self):
-#         # self.configure(bg=self.bg)
-#
-#         # Menu Bar for selecting theme
-#         self.menu_bar = tk.Menu(self)
-#         self.config(menu=self.menu_bar)
-#
-#         self.theme_menu = tk.Menu(self.menu_bar, tearoff=0)
-#         self.menu_bar.add_cascade(label="Theme", menu=self.theme_menu)
-#
-#         pixmap_themes = [
-#             "arc",
-#             "blue",
-#             "clearlooks",
-#             "elegance",
-#             "kroc",
-#             "plastik",
-#             "radiance",
-#             "winxpblue"
-#             ]
-#
-#         for theme in pixmap_themes:
-#             self.theme_menu.add_command(label=theme.capitalize() + " Mode",
-#                                         command=lambda
-#                                             t=theme: self.change_theme(t))
-#
-#         self.feature_tabs = ttk.Notebook(self)
-#         self.feature_tabs.pack(expand=True, fill=tk.BOTH)
-#         # bind click event to the theme menu
-#         # self.theme_menu.bind("<Button-1>", self.change_theme)
-#         # # Button Widgets
-#         # Def_Btn = tk.Button(self, text='Default Button')
-#         # Def_Btn.pack()
-#         # Themed_Btn = ttk.Button(self, text='Themed button')
-#         # Themed_Btn.pack()
-#         #
-#         # # Scrollbar Widgets
-#         # Def_Scrollbar = tk.Scrollbar(self)
-#         # Def_Scrollbar.pack(side='right', fill='y')
-#         # Themed_Scrollbar = ttk.Scrollbar(self, orient='horizontal')
-#         # Themed_Scrollbar.pack(side='top', fill='x')
-#
-#
-#         # Feature Tabs
-#         self.feature_tabs = ttk.Notebook(self)
-#         self.feature_tabs.pack(expand=True, fill=tk.BOTH)
-#
-#         # Data Information Tab
-#         self.data_information_tab = ttk.Frame(self.feature_tabs)
-#         self.feature_tabs.add(self.data_information_tab,
-#                               text="Data Information")
-#
-#         # Statistics Tab
-#         self.statistics_tab = ttk.Frame(self.feature_tabs)
-#         self.feature_tabs.add(self.statistics_tab, text="Statistics")
-#
-#         # Graph Tab
-#         self.graph_tab = ttk.Frame(self.feature_tabs)
-#         self.feature_tabs.add(self.graph_tab, text="Graph")
-#
-#     def init_home_page(self):
-#         # This function can be expanded for additional content on the home page
-#         pass
-#
-#     # def color_scheme(self, mode):
-#     #     if mode == 'dark':
-#     #         self.bg, self.fg, self.ac1, self.ac2 = (
-#     #         '#282828', 'white', '#404040y', '#B3B3B3')
-#     #     if mode == 'light':
-#     #         self.bg, self.fg, self.ac1, self.ac2 = (
-#     #         '#FBF8F1', 'black', '#F7ECDE', '#E9DAC1')
-#
-#     # def theme_switch(self):
-#         # window = ThemedTk(theme="arc")
-#         # if self.bg == '#282828':
-#         #     self.color_scheme('light')
-#         #     # We change the text to match the opposite theme
-#         #     self.theme_btn_text = 'Dark Theme'
-#         # elif self.bg == '#FBF8F1':
-#         #     self.color_scheme('dark')
-#         #     self.theme_btn_text = 'Light Theme'
-#         # if self.bg == '#282828':
-#         #     self.change_theme("light")
-#         # else:
-#         #     self.change_theme("dark")
-#         # self.restart_GUI()
-#
-#     def change_theme(self, theme):
-#         """Function to change the theme"""
-#         self.set_theme(theme)
-#
-#     def set_theme(self, theme):
-#         """Function to set the theme"""
-#         window = ThemedTk(theme=theme)
-#
-#     #     # Left Panel: Data Information
-#     #     self.left_frame = ttk.Frame(self, width=200, height=400)
-#     #     self.left_frame.pack(side=tk.LEFT, padx=20, pady=20, fill=tk.Y)
-#     #
-#     #     self.data_information_label = ttk.Label(self.left_frame,
-#     #                                             text="Data Information")
-#     #     self.data_information_label.pack(pady=10)
-#     #
-#     #     # self.data_information_image = tk.PhotoImage(
-#     #     #     file="data_information.png")
-#     #     # self.data_information_button = ttk.Button(self.left_frame,
-#     #     #                                           image=self.data_information_image,
-#     #     #                                           command=self.show_data_information)
-#     #     # self.data_information_button = ttk.Button(self.left_frame,
-#     #     #                                           command=self.show_data_information)
-#     #     self.data_information_cbb = ttk.Combobox(self.left_frame, values=["Summary Statistics", "Correlations"])
-#     #     self.data_information_cbb.pack(pady=10)
-#     #     # self.data_information_button.pack(pady=10)
-#     #
-#     #     # Middle Panel: Visualization
-#     #     self.middle_frame = ttk.Frame(self, width=400, height=400)
-#     #     # self.middle_frame.pack(side=tk.LEFT, padx=20, pady=20, fill=tk.Y)
-#     #     self.middle_frame.pack(side=tk.LEFT, padx=20, pady=20, fill=tk.BOTH, expand=True)
-#     #
-#     #     # self.visualization_image = tk.PhotoImage(
-#     #     #     file="data_information.png")
-#     #     self.visualization_label = ttk.Label(self.middle_frame,
-#     #                                          text="Visualization")
-#     #     self.visualization_label.pack(pady=10)
-#     #
-#     #     self.distribution_graph_button = ttk.Button(self.middle_frame,
-#     #                                                 text="Distribution Graph",
-#     #                                                 command=self.show_distribution_graph)
-#     #     self.distribution_graph_button.pack(pady=5, padx=10, fill=tk.X)
-#     #
-#     #     self.interactive_graph_button = ttk.Button(self.middle_frame,
-#     #                                                text="Interactive Graph",
-#     #                                                command=self.show_interactive_graph)
-#     #     self.interactive_graph_button.pack(pady=5, padx=10, fill=tk.X)
-#     #
-#     #     self.filtering_options_button = ttk.Button(self.middle_frame,
-#     #                                                text="Filtering Options",
-#     #                                                command=self.show_filtering_options)
-#     #     self.filtering_options_button.pack(pady=5, padx=10, fill=tk.X)
-#     #
-#     #     # Right Panel: Select
-#     #     self.right_frame = ttk.Frame(self, width=200, height=400)
-#     #     self.right_frame.pack(side=tk.LEFT, padx=20, pady=20, fill=tk.Y)
-#     #
-#     #
-#     #     self.select_label = ttk.Button(self.right_frame, text="More options")
-#     #     self.select_label.pack(pady=10, padx=10, fill=tk.X)
-#     #
-#     #     # self.select_label = ttk.Label(self.right_frame, text="Select")
-#     #     # self.select_label.pack()
-#     #     #
-#     #     # # self.select_image = tk.PhotoImage(file="select.png")
-#     #     # # self.select_button = ttk.Button(self.right_frame,
-#     #     # #                                 image=self.select_image,
-#     #     # #                                 command=self.show_select)
-#     #     # self.select_button = ttk.Button(self.right_frame,
-#     #     #                                 command=self.show_select)
-#     #     # self.select_button.pack(pady=10)
-#     #     # self.select_options = ttk.Combobox(self.right_frame, values=["Option 1", "Option 2", "Option 3"])
-#     #     # self.select_options.pack(pady=10)
-#     #     # self.select_cbb = ttk.Combobox(self.right_frame, command=self.show_select)
-#     #     # self.select_cbb.pack(pady=10)
-#     #
-#     # def show_data_information(self):
-#     #     """Function to handle Data Information click
-#     #     Show the options for Summary Statistics and Correlations"""
-#     #     pass
-#     #
-#     # def show_distribution_graph(self):
-#     #     """Function to display the default distribution graph
-#     #     (using Matplotlib)"""
-#     #     pass
-#     #
-#     # def show_interactive_graph(self):
-#     #     """ Function to allow users to select different interactive graphs
-#     #     Implement based on user's choice (e.g., scatter plots, boxplots)"""
-#     #     pass
-#     #
-#     # def show_filtering_options(self):
-#     #     """Function to allow users to apply filters to the data
-#     #     Implement based on user's selection criteria"""
-#     #     pass
-#     #
-#     # def show_select(self):
-#     #     """Function to handle Select click
-#     #     perform specific actions when Select is clicked"""
-#     #     pass
-#
-#     def run(self):
-#         self.mainloop()
+"""handles the User interface of the application"""
 
 import tkinter as tk
 from tkinter import ttk
@@ -222,7 +12,10 @@ class HeartDiseaseView(ThemedTk):
         self.controller = controller
         self.title("Heart Disease Explorer")
         self.init_components()
-        self.default_theme = 'clam'
+        # self.init_home_page()
+        self.geometry("800x600")
+        self.set_theme("winxpblue")
+        self.create_quit_button()
 
     def init_components(self):
         # Menu Bar for selecting theme
@@ -240,22 +33,30 @@ class HeartDiseaseView(ThemedTk):
             "Kroc Mode": "kroc",
             "Plastik Mode": "plastik",
             "Radiance Mode": "radiance",
-            "WinXPBlue Mode": "winxpblue",
+
+            "Black Mode": "black",
+            "Equilux Mode": "equilux",
         }
+        # "WinXPBlue Mode": "winxpblue",
 
         for theme_name, theme_value in themes.items():
             self.theme_menu.add_command(label=theme_name, command=lambda t=theme_value: self.change_theme(t))
 
-        self.theme_menu.add_command(label="Default Theme", command=self.set_default_theme)
-        self.bind("<Control-t>", lambda e: self.theme_switch())
+        self.theme_menu.add_command(label="Default Mode", command=self.set_default_theme)
 
         # Feature Tabs
         self.feature_tabs = ttk.Notebook(self)
-        self.feature_tabs.pack(expand=True, fill=tk.BOTH)
+        self.feature_tabs.configure(padding=10)
+        # self.feature_tabs.pack(side=tk.TOP,expand=True, fill=tk.BOTH)
+        # self.feature_tabs.pack(expand=True, fill=tk.BOTH)
+        self.feature_tabs.pack(fill=tk.BOTH, expand=True)
+        self.tabs()
 
+    def tabs(self):
         # Data Information Tab
         self.data_information_tab = ttk.Frame(self.feature_tabs)
         self.feature_tabs.add(self.data_information_tab, text="Data Information")
+        # self.configure(anchor=tk.CENTER)
 
         # Statistics Tab
         self.statistics_tab = ttk.Frame(self.feature_tabs)
@@ -265,38 +66,70 @@ class HeartDiseaseView(ThemedTk):
         self.graph_tab = ttk.Frame(self.feature_tabs)
         self.feature_tabs.add(self.graph_tab, text="Graph")
 
-        # Button to gracefully exit
-        self.quit_button = ttk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.pack()
-
     def change_theme(self, theme_value):
         self.set_theme(theme_value)
 
+        # Create a list to hold references to child widgets to be destroyed
+        widgets_to_destroy = []
         for child in self.winfo_children():
             if child != self.menu_bar and child != self.quit_button:
-                child.destroy()
+                widgets_to_destroy.append(child)
+
+        # Destroy the child widgets in the list
+        for widget in widgets_to_destroy:
+            widget.destroy()
+
+        # Reinitialize components to reflect the new theme
+        self.feature_tabs.destroy()
         self.init_components()
+        # self.init_components()
 
     def set_default_theme(self):
-        self.set_theme(self.default_theme)
+        # self.set_theme("aqua")
+        # # set to be centered
+        # self.geometry("800x600")
+        #
+        # # Create a list to hold references to child widgets to be destroyed
+        # widgets_to_destroy = []
+        # for child in self.winfo_children():
+        #     if child != self.menu_bar and child != self.quit_button:
+        #         widgets_to_destroy.append(child)
+        #
+        # # Destroy the child widgets in the list
+        # for widget in widgets_to_destroy:
+        #     widget.destroy()
+        #
+        # # Reinitialize components to reflect the default theme
+        # self.init_components()
+        # Disable the default theme
 
-        for child in self.winfo_children():
-            if child != self.menu_bar and child != self.quit_button:
-                child.destroy()
+        # Set new default theme to winxpblue
+        self.set_theme("winxpblue")
+
+        # Set the background color
+        # self.configure(background=self.style.lookup(".", "background"))
+
+
+
+        # Destroy and reinitialize components to reflect the default theme
+        widgets_to_destroy = [child for child in self.winfo_children() if
+                              child not in [self.menu_bar, self.quit_button]]
+        for widget in widgets_to_destroy:
+            widget.destroy()
         self.init_components()
+
+    def create_quit_button(self):
+        # Button to gracefully exit
+        self.quit_button = ttk.Button(self, text="Quit", command=self.quit)
+        self.quit_button.pack(side=tk.BOTTOM, padx=5, pady=5)
 
     def init_home_page(self):
         # This function can be expanded for additional content on the home page
         pass
 
-    # def theme_switch(self):
-    #     # Display current theme in a dialog (optional):
-    #     current_theme = self.theme_get()
-    #     message = f"Current Theme: {current_theme.capitalize()}"
-    #     tk.messagebox.showinfo(title="Theme Information", message=message)
-
     def run(self):
         self.mainloop()
+
 
 if __name__ == "__main__":
     controller = ...  # Initialize your controller object
--- code/view2.py
+++ code/view2.py
@@ -16,52 +16,71 @@ class HeartDiseaseView(customtkinter.CTk):
         self.init_components()
 
     def init_components(self):
+        self.tabview = customtkinter.CTkTabview(self, width=250)
+        self.tabview.pack(side="left", fill="both", expand=True)
+        self.tabview.add("CTkTabview")
+        self.tabview.add("Tab 2")
+        self.tabview.add("Tab 3")
+        self.tabview.tab("CTkTabview").grid_columnconfigure(0,
+                                                            weight=1)  # configure grid of individual tabs
+        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
         # Menu Bar for Selecting theme
-        self.menu_bar = tk.Menu(self)
-        self.config(menu=self.menu_bar)
-        self.theme_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.theme_menu.add_command(label="System", command=self.set_system_theme)
-        self.theme_menu.add_command(label="Light", command=self.set_light_theme)
-        self.theme_menu.add_command(label="Dark", command=self.set_dark_theme)
-        self.menu_bar.add_cascade(label="Theme", menu=self.theme_menu)
-        self.menu_bar.add_command(label="Quit", command=self.quit)
-        self.menu_bar.bind("<Control-q>", lambda e: self.quit())
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(
+            self.tabview.tab("CTkTabview"), dynamic_resizing=False,
+            values=["Value 1", "Value 2", "Value Long Long Long"])
+        self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.combobox_1 = customtkinter.CTkComboBox(
+            self.tabview.tab("CTkTabview"),
+            values=["Value 1", "Value 2", "Value Long....."])
+        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
+
+        # self.menu_bar = customtkinter.CTkOptionMenu(self, values=["System", "Light", "Dark"],
+        #                                             command=lambda mode: customtkinter.set_appearance_mode(mode))
+        # self.optionmenu.configure(menu=self.menu_bar)
+        # self.config(menu=self.menu_bar)
+        # self.theme_menu = tk.Menu(self.menu_bar, tearoff=0)
+        # self.theme_menu.add_command(label="System", command=self.set_system_theme)
+        # self.theme_menu.add_command(label="Light", command=self.set_light_theme)
+        # self.theme_menu.add_command(label="Dark", command=self.set_dark_theme)
+        # self.menu_bar.add_cascade(label="Theme", menu=self.theme_menu)
+        # self.menu_bar.add_command(label="Quit", command=self.quit)
+        # self.menu_bar.bind("<Control-q>", lambda e: self.quit())
 
         # Feature Tabs
-        self.feature_tabs = ttk.Notebook(self)
-        self.feature_tabs.pack(expand=True, fill=tk.BOTH)
-
-        # Data Information Tab
-        self.data_information_tab = ttk.Frame(self.feature_tabs)
-        self.feature_tabs.add(self.data_information_tab,
-                              text="Data Information")
-
-        # Statistics Tab
-        self.statistics_tab = ttk.Frame(self.feature_tabs)
-        self.feature_tabs.add(self.statistics_tab, text="Statistics")
-
-        # Graph Tab
-        self.graph_tab = ttk.Frame(self.feature_tabs)
-        self.feature_tabs.add(self.graph_tab, text="Graph")
-
-        # Button to gracefully exit
-        self.quit_button = ttk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.pack()
-
-    @staticmethod
-    def set_system_theme():
-        customtkinter.set_appearance_mode("System")
-        show_toast("Changed theme color to System")
-
-    @staticmethod
-    def set_light_theme():
-        customtkinter.set_appearance_mode("light")
-        show_toast("Changed theme color to Light")
-
-    @staticmethod
-    def set_dark_theme():
-        customtkinter.set_appearance_mode("dark")
-        show_toast("Changed theme color to Dark")
+    #     self.feature_tabs = ttk.Notebook(self)
+    #     self.feature_tabs.pack(expand=True, fill=tk.BOTH)
+    #
+    #     # Data Information Tab
+    #     self.data_information_tab = ttk.Frame(self.feature_tabs)
+    #     self.feature_tabs.add(self.data_information_tab,
+    #                           text="Data Information")
+    #
+    #     # Statistics Tab
+    #     self.statistics_tab = ttk.Frame(self.feature_tabs)
+    #     self.feature_tabs.add(self.statistics_tab, text="Statistics")
+    #
+    #     # Graph Tab
+    #     self.graph_tab = ttk.Frame(self.feature_tabs)
+    #     self.feature_tabs.add(self.graph_tab, text="Graph")
+    #
+    #     # Button to gracefully exit
+    #     self.quit_button = ttk.Button(self, text="Quit", command=self.quit)
+    #     self.quit_button.pack()
+    #
+    # @staticmethod
+    # def set_system_theme():
+    #     customtkinter.set_appearance_mode("System")
+    #     show_toast("Changed theme color to System")
+    #
+    # @staticmethod
+    # def set_light_theme():
+    #     customtkinter.set_appearance_mode("light")
+    #     show_toast("Changed theme color to Light")
+    #
+    # @staticmethod
+    # def set_dark_theme():
+    #     customtkinter.set_appearance_mode("dark")
+    #     show_toast("Changed theme color to Dark")
 
 
 def show_toast(message):
@@ -106,3 +125,48 @@ def show_toast(message):
 # button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 app = HeartDiseaseView(None)
 app.mainloop()
+
+# import tkinter as tk
+# import tkinter.ttk as ttk
+#
+#
+# def darkstyle(root):
+#     """Return a dark style to the window"""
+#     style = ttk.Style(root)
+#     # root.tk.call('source', 'azure dark/azure dark.tcl')
+#     # style.theme_use('azure')
+#     style.theme_use('clam')
+#     style.configure('.', background='black')
+#     style.configure("Accentbutton", foreground='white')
+#     style.configure("Togglebutton", foreground='white')
+#     return style
+#
+#
+# def main_window():
+#     """ The window with the darkstyle """
+#     root = tk.Tk()
+#     root.title("My App")
+#     root.resizable(False, False)
+#     img = tk.PhotoImage(file="select.png")
+#
+#     style = darkstyle(root)
+#
+#     lab = ttk.Label(
+#         root,
+#         text="Hello World",
+#         compound="center",
+#         font="arial 50",
+#         image=img)
+#     lab.pack(fill="both", expand=1)
+#
+#     button = ttk.Button(
+#         root,
+#         text="Click me"
+#     )
+#
+#     button.place(relx=0.43, rely=0.7, width=100, height=30)
+#
+#     root.mainloop()
+#
+#
+# main_window()
