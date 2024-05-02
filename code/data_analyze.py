5cb7aa59f62983d561e07573b2aa07b545a18fcb
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-05-03 00:29:37 +0700

    DI tab can show info

--- code/data_analyze.py
+++ code/data_analyze.py
@@ -3,58 +3,75 @@ import seaborn as sns
 import pandas as pd
 from model import HeartDiseaseModel
 
-# data = pd.read_csv('heart.csv')
+# pd.read_csv('heart.csv')
 # print([i for i in data.columns])
 
 # Load the data
-heart_disease_model = HeartDiseaseModel()
-data = heart_disease_model.data
+# heart_disease_model = HeartDiseaseModel()
+# data = heart_disease_model.data
 
-# Iterate through each column
-for column in data.columns:
-    # # Create a new figure for each plot
-    # plt.figure()
+
+# def show_data_information(data):
+#     """Show the data information."""
+#     print("Data Information")
+#     print(data.info())
+#     print("\n")
+#
+#
+# show_data_information(data)
+
+# def plot_correlation_heatmap(data):
+    # # Calculate correlations
+    # correlations = data.corr()
     #
-    # # Plot the distribution of data in the column using seaborn's distplot
-    # sns.histplot(data[column], kde=True)
-    # plt.title(f'Distribution of {column}')
-    # plt.xlabel(column)
-    # plt.ylabel('Frequency')
-    #
-    # # Show the plot
+    # # Create a heatmap of the correlations
+    # plt.figure(figsize=(10, 8))
+    # sns.heatmap(correlations, annot=True, cmap="coolwarm")
+    # plt.title("Correlation Heatmap")
     # plt.show()
 
-    max_value = data[column].max()
-    min_value = data[column].min()
-    iqr_value = data[column].quantile(0.75) - data[column].quantile(0.25)
-    data_range = max_value - min_value
-    std_dev = data[column].std()
-    variance = data[column].var()
-    mode_value = data[column].mode().values[0] if data[column].dtype == 'object' else None  # Mode for nominal data
 
-    # Plot histograms
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    sns.histplot(data[column], kde=True)
-    plt.title(f'Histogram of {column}')
-    plt.xlabel(column)
+# plot_correlation_heatmap(data)
 
-    # Plot boxplots
-    plt.subplot(1, 2, 2)
-    sns.boxplot(data[column])
-    plt.title(f'Boxplot of {column}')
-    plt.xlabel(column)
-    plt.tight_layout()
+class DataAnalyze:
+    """Class for analyzing data."""
 
-    # Print statistics
-    print(f"Column: {column}")
-    print(f"Max: {max_value}")
-    print(f"Min: {min_value}")
-    print(f"IQR: {iqr_value}")
-    print(f"Range: {data_range}")
-    print(f"Standard Deviation: {std_dev}")
-    print(f"Variance: {variance}")
-    print(f"Mode: {mode_value}\n")
+    def __init__(self):
+        self.data = pd.read_csv('heart.csv')
 
-    # Show plots
-    plt.show()
+    def get_column_names(self):
+        """Return the column names."""
+        return self.data.columns.tolist()
+
+
+# class DataInfomationWorker:
+#     """Class for showing in Data Information tab."""
+
+    def summary_statistics(self, column):
+        """Return the summary statistics."""
+        return self.data[column].describe()
+
+        # if choice == 'mean':
+        #     return df[column].mean()
+        # elif choice == 'median':
+        #     return df[column].median()
+        # elif choice == 'mode':
+        #     return df[column].mode()
+        # elif choice == 'std':
+        #     return df[column].std()
+        # elif choice == 'min':
+        #     return df[column].min()
+        # elif choice == 'max':
+        #     return df[column].max()
+        # elif choice == 'count':
+        #     return df[column].count()
+        # elif choice == 'unique':
+        #     return df[column].unique()
+        # elif choice == 'nunique':
+        #     return df[column].nunique()
+        # else:
+        #     return 'Invalid choice'
+
+    def correlations(self, df):
+        """Return the correlations."""
+        return df.corr()
--- code/view.py
+++ code/view.py
@@ -3,8 +3,9 @@
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
-from model import *  # Assuming this imports your model logic
+from model import *
 import time
+from data_analyze import DataAnalyze
 
 
 class HeartDiseaseView(ThemedTk):
@@ -18,20 +19,51 @@ class HeartDiseaseView(ThemedTk):
         self.geometry("1000x600")
         self.create_quit_button()
 
+
     def init_components(self):
         # Create the left panel frames
-
         self.left_panel1 = ttk.Frame(self, width=150, height=600)  # relief="solid",
-        self.left_panel2 = ttk.Frame(self, width=200, height=600)  # relief="solid", bg="white"
+        # self.left_panel2 = ttk.Frame(self, width=200, height=600)  # relief="solid", bg="white"
 
-        title_label = ttk.Label(self.left_panel1, text="History", font=("TkDefaultFont", 12, "bold"))
-        title_label.pack(side="top", fill="x", padx=5, pady=5)
+        # # Create checkboxes for column selection
+        # self.column_selection_frame = ttk.LabelFrame(self.left_panel1,
+        #                                              text="Column Selection")
+        # self.column_selection_frame.pack(fill="both", expand=True)
+        #
+        # self.column_selection_vars = {}
+        # self.column_selection_checkboxes = {}
+        # columns = ["Column1", "Column2",
+        #            "Column3"]
+        #
+        # for column in columns:
+        #     var = tk.BooleanVar()
+        #     self.column_selection_vars[column] = var
+        #     checkbox = ttk.Checkbutton(self.column_selection_frame,
+        #                                text=column, variable=var)
+        #     checkbox.pack(anchor="w")
+        #     self.column_selection_checkboxes[column] = checkbox
 
-        self.left_panel1.pack(side="left", fill="y", padx=5, pady=5)
-        self.left_panel2.pack(side="left", fill="y", padx=5, pady=5)
+        # title_label = ttk.Label(self.left_panel1, text="History", font=("TkDefaultFont", 12, "bold"))
+        # title_label.pack(side="top", fill="x", padx=5, pady=5)
+        # Add a label for the combobox
+        attributes_label = ttk.Label(self.left_panel1, text="Select Attribute:")
+        attributes_label.pack(side="top", fill="x", padx=5, pady=5)
 
-        self.left_panel1.pack_propagate(False)
-        self.left_panel2.pack_propagate(False)
+        # Add a combobox to select attributes
+        self.attribute_combobox = ttk.Combobox(self.left_panel1,
+                                               values=["Attribute 1",
+                                                       "Attribute 2",
+                                                       "Attribute 3"])
+        self.attribute_combobox.pack(side="top", fill="x", padx=5, pady=5)
+        self.attribute_combobox.set("Attribute 1")  # Set default value
+        self.attribute_combobox.bind("<<ComboboxSelected>>", lambda event: self.handle_attribute_selection(self.attribute_combobox.get()))
+
+
+        # self.left_panel1.pack(side="left", fill="y", padx=5, pady=5)
+        # # self.left_panel2.pack(side="left", fill="y", padx=5, pady=5)
+        #
+        # self.left_panel1.pack_propagate(False)
+        # self.left_panel2.pack_propagate(False)
 
         # Menu Bar for selecting theme
         self.menu_bar = tk.Menu(self)
@@ -68,10 +100,15 @@ class HeartDiseaseView(ThemedTk):
         self.tabs()
 
     def tabs(self):
+        """Create tabs for the application."""
+        # Home Tab
+        self.home_tab = ttk.Frame(self.feature_tabs)
+        self.feature_tabs.add(self.home_tab, text="Home")
+
         # Data Information Tab
         self.data_information_tab = ttk.Frame(self.feature_tabs)
         self.feature_tabs.add(self.data_information_tab, text="Data Information")
-        # self.configure(anchor=tk.CENTER)
+        self.init_data_information_tab()
 
         # Statistics Tab
         self.statistics_tab = ttk.Frame(self.feature_tabs)
@@ -81,13 +118,99 @@ class HeartDiseaseView(ThemedTk):
         self.graph_tab = ttk.Frame(self.feature_tabs)
         self.feature_tabs.add(self.graph_tab, text="Graph")
 
+    def init_data_information_tab(self):
+        """Initialize the Data Information tab."""
+        # Add a label for the combobox in Data Information tab
+        attributes_label = ttk.Label(self.data_information_tab,
+                                     text="Select Attribute:")
+        attributes_label.pack(side="top", fill="x", padx=5, pady=5)
+
+        # column = pd.read_csv('heart.csv').columns
+        column = DataAnalyze().get_column_names()
+
+        # Add a combobox to select attributes in Data Information tab
+        self.attribute_combobox = ttk.Combobox(self.data_information_tab,
+                                               values=[i for i in column])
+        self.attribute_combobox.pack(side="top", fill="x", padx=5, pady=5)
+        self.attribute_combobox.set(column[0])  # Set default value
+        self.attribute_combobox.bind("<<ComboboxSelected>>", lambda
+            event: self.handle_attribute_selection(
+            self.attribute_combobox.get()))
+    # def data_information_tab1(self):
+    #     self.data_info_vars = {}
+    #     self.data_info_columns = self.get_data_columns()  # Assuming this gets column names from CSV
+    #
+    #     # Add a label for the combobox in Data Information tab
+    #     self.data_information_tab = ttk.Frame(self.feature_tabs)
+    #     self.feature_tabs.add(self.data_information_tab,
+    #                           text="Data Information")
+    #     attributes_label = ttk.Label(self.data_information_tab,
+    #                                  text="Select Attribute:")
+    #     attributes_label.pack(side="top", fill="x", padx=5, pady=5)
+    #
+    #     # Add a combobox to select attributes in Data Information tab
+    #     self.attribute_combobox = ttk.Combobox(self.data_information_tab,
+    #                                            values=self.data_info_columns)
+    #     self.attribute_combobox.pack(side="top", fill="x", padx=5, pady=5)
+    #     self.attribute_combobox.set(
+    #         self.data_info_columns[0])  # Set default value
+    #
+    #     self.left_panel1.pack(side="left", fill="y", padx=5, pady=5)
+    #     self.left_panel1.pack_propagate(False)
+
+    def handle_attribute_selection(self, selected_attribute):
+        """Handle the selection of an attribute in the combobox of the Data Information tab."""
+        # Get data analyzer instance
+        data_analyzer = DataAnalyze()
+
+        # Retrieve data information for the selected attribute
+        data_info = data_analyzer.summary_statistics(selected_attribute)
+
+        # Update the UI in the "Data Information" tab to display retrieved information
+        self.update_data_information_tab(data_info)
+
+    def update_data_information_tab(self, data_info):
+        """Update the Data Information tab with the given data information."""
+        for stat, value in data_info.items():
+            # Check if a label for this statistic already exists
+            existing_label = None
+            for child in self.data_information_tab.winfo_children():
+                if isinstance(child, ttk.Label) and child.cget(
+                        "text").startswith(stat + ":"):
+                    existing_label = child
+                    break
+
+            # If an existing label exists, update its text
+            if existing_label:
+                existing_label.config(text=f"{stat}: {value}")
+            # Otherwise, create a new label
+            else:
+                stat_label = ttk.Label(self.data_information_tab,
+                                       text=f"{stat}: {value}")
+                stat_label.pack(side="top", fill="x", padx=5, pady=2)
+        # Clear any existing content in the tab
+        # for widget in self.data_information_tab.winfo_children():
+        #     widget.destroy()
+        #
+        #
+        #
+        # # Display the statistics in the tab
+        # for stat, value in data_info.items():
+        #     stat_label = ttk.Label(self.data_information_tab,
+        #                            text=f"{stat}: {value}")
+        #     stat_label.pack(side="top", fill="x", padx=5, pady=2)
+
+        # self.attribute_combobox.destroy()
+
     def tabs_left_panel1(self):
+        """Create tabs for the application."""
         # Add widgets to the first left panel
         label_left_panel = tk.Label(self.left_panel, text="Left Panel",
                                     bg="white", padx=10, pady=5)
         label_left_panel.pack()
 
     def change_theme(self, theme_value):
+        """Change the theme of the application."""
         self.set_theme(theme_value)
 
         # Create a list to hold references to child widgets to be destroyed
@@ -109,10 +232,13 @@ class HeartDiseaseView(ThemedTk):
         # self.bind("<Configure>", self.show_theme_change_popup(theme_value))
 
     def show_theme_change_popup(self, new_theme):
+        """Show a pop-up message indicating that the theme has been changed."""
         popup_message = f"Theme changed to {new_theme}"
         self.pop_up = Toast(self, popup_message, duration=2000)
 
     def set_default_theme(self):
+        """Set the default theme of the application."""
+        # commented code below set aqua as a default.
         # self.set_theme("aqua")
         # # set to be centered
         # self.geometry("800x600")
@@ -133,8 +259,6 @@ class HeartDiseaseView(ThemedTk):
 
         # Set new default theme to winxpblue
         self.set_theme("winxpblue")
-        # self.bind("<Configure>", self.show_theme_change_popup("Default Theme Set"))
-        # time.sleep(2)
 
         # Destroy and reinitialize components to reflect the default theme
         widgets_to_destroy = [child for child in self.winfo_children() if
@@ -175,27 +299,3 @@ class Toast(tk.Toplevel):
         self.geometry("+{}+{}".format(self.x, self.y))
         self.deiconify()
         self.after(self.duration, self.destroy)
-# class Toast(tk.Toplevel):
-#     def __init__(self, parent, message, duration=2000):
-#         tk.Toplevel.__init__(self, parent)
-#         self.overrideredirect(True)
-#         self.attributes("-topmost", True)
-#         self.parent = parent
-#         self.message = message
-#         self.duration = duration
-#         self.label = tk.Label(self, text=self.message, bg="black", fg="white", padx=10, pady=5)
-#         self.label.pack()
-#         self.update_idletasks()
-#         self.width = self.winfo_width()
-#         self.height = self.winfo_height()
-#         self.x = self.parent.winfo_x() + (self.parent.winfo_width() // 2) - (self.width // 2)
-#         self.y = self.parent.winfo_y() + self.parent.winfo_height() - self.height - 20
-#         self.geometry("+{}+{}".format(self.x, self.y))
-#         self.deiconify()
-#         self.after(self.duration, self.destroy)
-
-
-if __name__ == "__main__":
-    controller = ...  # Initialize your controller object
-    window = HeartDiseaseView(controller)
-    window.mainloop()
